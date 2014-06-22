c1ab553a32109a0118eaad536d48e9266ec9ecb2
soyuka <soyuka@gmail.com>
2014-06-22 14:49:57 +0200

    Improved binding

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = __dirname
+module.exports = function(shell) {
+  if(['install', 'useradd', 'userdel', 'daemon'].indexOf(shell) !== -1)
+    return p.join(__dirname, shell) + '.sh'
+  else
+    throw new Error('Shell %s does not exists', shell)
+}
