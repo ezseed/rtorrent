d3e882fc97d56c748033a80716c9bceed0cee0f6
soyuka <soyuka@gmail.com>
2014-10-03 08:05:39 +0200

    fix(): Missing passwd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var p = require('path')
 
 module.exports = function(shell) {
-  if(['install', 'useradd', 'userdel', 'daemon'].indexOf(shell) !== -1)
+  if(['install', 'useradd', 'userdel', 'daemon', 'passwd'].indexOf(shell) !== -1)
     return p.join(__dirname, shell) + '.sh'
   else
     throw new Error('Shell %s does not exists', shell)
